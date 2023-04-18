8b9fdb0d3e84bf5f3679ec230f15e63ab18de2aa
GianlucaCobucci <gianlucacobucci330@gmail.com>
2023-04-18 18:16:00 +0200

    Cambiamenti minori

--- src/components/MyBadge.js
+++ src/components/MyBadge.js
@@ -3,9 +3,11 @@ import Badge from 'react-bootstrap/Badge';
 
 
 const MyBadge = ({str, color}) => {
+
   return (
     <Badge bg={color}>{str}</Badge>
   )
+
 }
 
 export default MyBadge
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -3,11 +3,15 @@ import Container from 'react-bootstrap/Container';
 /* non riesco ad importare il Jumbotron da nessuna libreria*/
 
 const Welcome = () => {
+
   return (
-      <Container>
-        <h1 className="text-center">EpiBooks</h1>
-      </Container>
+
+    <Container>
+      <h1 className="text-center">EpiBooks</h1>
+    </Container>
+
   );
+  
 };
 
 export default Welcome;
