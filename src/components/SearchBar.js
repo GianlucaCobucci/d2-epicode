e38448422293edcbeacbfd82d79a17c1c8de3565
GianlucaCobucci <gianlucacobucci330@gmail.com>
2023-04-18 15:56:15 +0200

    Cambiamenti minori

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,34 +1,44 @@
-import React,{useState} from 'react'
+import React, { useState } from "react";
 import { Button, Col, Container, Form, Row } from "react-bootstrap";
 
-const SearchBar = ({books, setBooks}) => {
+const SearchBar = ({ books = [], setBooks }) => {
 
-    const [searchTerm, setSearchTerm] = useState("")
-    console.log(searchTerm)
+  const [searchTerm, setSearchTerm] = useState("");
+  console.log(searchTerm);
 
-    const filteredBooks = {}
-
-
-    return (
-      <Container className="mt-5">
-        <Row>
-          <Col sm={12}>
-            <Form className="d-flex">
-              <Form.Control
-                onChange={(e) => setSearchTerm(e.target.value)}
-                type="search"
-                placeholder="Search"
-                className="me-2"
-                aria-label="Search"
-              />
-              <Button>
-                Cerca
-              </Button>
-            </Form>
-          </Col>
-        </Row>
-      </Container>
+  const handleSearch = () => {
+    const filteredBooks = books.filter((book) =>
+      book.title.toLowerCase().includes(searchTerm.toLowerCase())
     );
-  }
+    setBooks(filteredBooks);
+  };
 
-export default SearchBar
+  return (
+    <Container className="mt-5">
+      <Row>
+        <Col sm={12}>
+          <Form className="d-flex">
+            <Form.Control
+              onChange={(e) => setSearchTerm(e.target.value)}
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button onClick={handleSearch}>Cerca</Button>
+          </Form>
+        </Col>
+      </Row>
+      <Row>
+        {books.map((book) => (
+          <Col key={book.id} md={3}>
+            <h4>{book.title}</h4>
+            <p>{book.author}</p>
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
+};
+
+export default SearchBar;
